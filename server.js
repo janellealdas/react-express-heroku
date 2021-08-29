7fc73c512be2c501e46d24173447a97813b64535
janellealdas <63031648+janellealdas@users.noreply.github.com>
2021-08-29 15:57:34 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.json({ extended: false }));
+
+app.use('/api/visited-places', require('./routes/api/visited-places'));
+app.use(
+  '/api/social-interactions',
+  require('./routes/api/social-interactions')
+);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
